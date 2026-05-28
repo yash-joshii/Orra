a849748986a345ea0bbead5f14438fe995f35391
Yash Joshi <yashjoshi.yj786@gmail.com>
2026-05-28 15:02:11 +0530

    initialize springboot setup

---
+++ Orra-Backend-SpringBoot/Orrabackend/src/main/java/com/orra/Orrabackend/OrrabackendApplication.java
@@ -0,0 +1,13 @@
+package com.orra.Orrabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrrabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrrabackendApplication.class, args);
+	}
+
+}
