cbbf62ddb98e335209c3a4c06d79aa762a2a287e
Sushma Kure <sushmak@bsf.io>
2026-07-28 13:19:29 +0530

    Implement wishlist feature with global exception handling

---
+++ Orra-Backend-SpringBoot/Orrabackend/src/main/java/com/orra/Orrabackend/exception/DuplicateWishlistException.java
@@ -0,0 +1,8 @@
+package com.orra.Orrabackend.exception;
+
+public class DuplicateWishlistException extends RuntimeException {
+
+    public DuplicateWishlistException(String message) {
+        super(message);
+    }
+}
--- Orra-Backend-SpringBoot/Orrabackend/src/main/java/com/orra/Orrabackend/exception/UserNotFoundException.java
+++ Orra-Backend-SpringBoot/Orrabackend/src/main/java/com/orra/Orrabackend/exception/UserNotFoundException.java
@@ -1,7 +1,8 @@
 package com.orra.Orrabackend.exception;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("User Not Found With ID : " + id);
+    public UserNotFoundException(String id){
+
+        super(id);
     }
 }
